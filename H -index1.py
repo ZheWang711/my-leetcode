76affe30e6f14235ec13642ba72bbae34c202e15
zhewang711 <zhw176@ucsd.edu>
2015-09-23 13:16:58 -0700

    Leetcode 274 H-Index

--- H -index1.py
+++ H -index1.py
@@ -5,14 +5,14 @@ class Solution(object):
     def hIndex(self, citations):
         tmp = sorted(citations)
         for i in range(len(tmp) - 1, -1, -1):
-            # 1. sort array A ==> for all i : A[i] <= a[i + 1]
-            # 2.scan A from right to left to find the largest i such that
+            # 1. sort array A ==> for all i : A[i] <= A[i + 1]
+            # 2. scan A from right to left to find the left most i such that y <= A[i] where
             #   y = len(A) - i is the # of elements whose value is larger than or equal to A[i].
-            #   Since a[index] >= y,
-            #   there are y elements in array A whose value is larger than a[index] and larger than y,
-            #   which means that y is a possible H-index.
-            # In the y + 1 most largest elements (from a[i-1] to a[-1]),
-            # because a[i-1] < y + 1, there are only y elements whose
+            # Since A[i] >= y,
+            # there are y elements in array A whose value is larger than A[i] thus larger than y,
+            # which means that y is a possible H-index.
+            # Among the y + 1 most largest elements (from a[i-1] to a[-1]),
+            # notice that A[i-1] < y + 1 (A[i] is the left most element whose value >= y), so there are only y elements whose
             # value might >= y + 1, so y + 1 is not a possible H-index.
             # Since y is a possible H-index and y + 1 is not a possible H-index, y is the final H-index.
             y = len(tmp) - i
